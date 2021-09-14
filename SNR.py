080a62e224d77e7c042b6199b1df6d80fcc223fb
sean-cunniffe <A00290418@student.ait.ie>
2021-09-14 08:43:18 +0100

    Signal noise ratio in SNR.py

--- SNR.py
+++ SNR.py
@@ -1,10 +1,7 @@
-#Simple script to calculate the Signal to Noise Ratio (SNR) based on
-#the Shannon-Hartley theorm.
 import math as m
-C = float(input("Enter the channel capacity in bps: "))
-B = float(input("Enter the available bandwidth in Hz: "))
-SN = m.pow(2.0, (C/B))-1
-SNR = 10*m.log10(SN)
-print('Signal to Noise Ratio = %f db ' %SNR)
-print('Signal to Noise Ratio = %s db ' %format(SNR, '0.3E'))
- 
+
+capacity = int(input("What is the channel capacity? (bits/s)"))
+bandwidth = int(input("What is the bandwidth? (hertz)"))
+sn = m.pow(2, capacity / bandwidth) - 1
+ratio_in_db = 10 * (m.log(sn, 10))
+print('SNR = %f db' % ratio_in_db)
